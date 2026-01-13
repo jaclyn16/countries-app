5f018c81a799a7152fcd366e38e1106698cff75c
jaclyn16 <jgrix16@yahoo.com>
2026-01-13 12:15:18 -0800

    added a form to version-1 and styling to countrydetails card..

--- version-1/src/pages/CountryDetails.jsx
+++ version-1/src/pages/CountryDetails.jsx
@@ -1,27 +1,36 @@
-import { useParams } from "react-router-dom";
-// this import useParams lets us read the id from the url
+import { useParams, useNavigate } from "react-router-dom";
+// this import useParams lets us read the id from the url and useNavigate lets user move/back to another page
 
 // this function is the countryDetails page component..
-// it receives the full list of countriesData as a prop...
-// line 9..const grabs the id from the url..
-// line 10....const finds country thta matches id
+// it receives the full list of countriesData array as a prop from app.jsx...
+// line 9..const grabs the id values from the url..
+// line 12....const finds country that matches id..searches through the data array
 function CountryDetails({ countriesData }) {
     const { id } = useParams();
+    const navigate = useNavigate(); 
+// line 10 creates navigate function inside component to change pages...
     const country = countriesData.find(
         (c) => c.cca3 === id || c.name.common === id
     );
-// if/else statement....if we couldn't find the country,show a message
+// if/else statement....if we couldn't find the country no data found,show a message
     if (!country) {
         return <p>Country not found.</p>
     }
 // if we DID find it, it will show these details....
     return (
         <main className="page">
-           <h1 className="page-title">{country.name.common}</h1>
-            <img className="country-flag" src={country.flags.png} alt={`Flag of ${country.name.common}`} />
-            <p>Population: {country.population}</p>
-            <p>Region: {country.region}</p>
-                <p>Capital: {country.capital?.[0]}</p>
+            <button className="back-btn" type="button" onClick={() => navigate(-1)}>Back</button>
+            <div className="details-layout">      
+            <img className="detail-flag" src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+           <div className="details-info"> 
+                    <h2 className="details-title">{country.name.common}</h2>
+           <button className="save-btn" type="button">Save</button>
+           
+            <p className="details-text">Population: {country.population}</p>
+            <p className="details-text">Region: {country.region}</p>
+            <p className="details-text"> Capital: {country.capital?.[0]}</p>
+            </div>
+            </div>
        </main>
 
     );
--- version-1/src/pages/SavedCountries.jsx
+++ version-1/src/pages/SavedCountries.jsx
@@ -1,10 +1,49 @@
-// this page is for saved countries...not done yet..will have a page title,
-// and the coming soon= a placeholder text for now..
+// useState lets us st6ore and update the form input values
+import { useState } from "react";
+// this is a component the renders the saved countries page..
+// the const formData stores all the the input values into 1 object..
+// each key "" matches the inputs name attribute...
 function SavedCountries() {
+    const [formData, setFormData] = useState({
+        fullName: "",
+        email: "",
+        country: "",
+        bio: "",
+    });
+    // this function runs everytime the user types in the input..
+    // the setFormData..updates only when being changed, when the form data stays the same..
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+    }
+    // this function runs when the form is submitted...
+    // the e.prevent stops page from refreshing
+    function handleSubmit(e) {
+        e.preventDefault();
+// this just resets the form after was submitted
+        setFormData({
+            fullName: "",
+            email: "",
+            country: "",
+            bio: "",
+        });
+    }
+    // this is the jsx that returns what to be displayed on page..
     return (
         <main className="page">
-            <h1 className="page-title">Saved Countries</h1>
-            <p>Coming Soon.</p>
+            <h3 className="page-title">My Saved Countries</h3>
+            <section className="saved-layout">
+                <div className="profile-card">
+                    <h2 className="section-title">My Profile</h2>
+                    <form className="profile-form" onSubmit={handleSubmit}>
+                        <input type="text" name="fullName" placeholder="Full Name" value={formData.fullName} onChange={handleChange}/>
+                        <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange}/>
+                    <input type="text" name="country" placeholder="Country" value={formData.country} onChange={handleChange}/>
+                    <textarea name="bio" placeholder="Bio" rows="6" value={formData.bio} onChange={handleChange}/>
+                    <button className="submit-btn" type="submit">Submit</button>
+                    </form>
+                </div>
+            </section>
         </main>
     );
 }
