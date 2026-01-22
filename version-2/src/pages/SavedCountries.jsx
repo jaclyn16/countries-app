42ac928c02b590405498943c0d6c9528d90d4b36
jaclyn16 <jgrix16@yahoo.com>
2026-01-22 14:51:10 -0800

    added last user to saved countries

--- version-2/src/pages/SavedCountries.jsx
+++ version-2/src/pages/SavedCountries.jsx
@@ -1,21 +1,66 @@
 // useState lets us st6ore and update the form input values
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // this is a component the renders the saved countries page..
 // the const formData stores all the the input values into 1 object..
 // each key "" matches the inputs name attribute...
-function SavedCountries() {
+function SavedCountries({ countriesData }) {
     const [formData, setFormData] = useState({
         fullName: "",
         email: "",
         country: "",
         bio: "",
     });
+    const [newestUserData, setNewestUserData] = useState(null);
+    const [savedCountries, setSavedCountries] = useState([]);
     // this function runs everytime the user types in the input..
     // the setFormData..updates only when being changed, when the form data stays the same..
     function handleChange(e) {
         const { name, value } = e.target;
         setFormData((prev) => ({ ...prev, [name]: value }));
     }
+    const getSavedCountriesData = async () => {
+        try {
+            const response = await fetch(
+                "https://backend-answer-keys.onrender.com/get-all-saved-countries",
+                {
+                    method: "GET",
+                }
+            );
+            const data = await response.json();
+            setSavedCountries(data);
+        } catch (error) {
+            console.log(error);
+            }
+    };
+     useEffect(() => {
+    getSavedCountriesData();
+  }, []);
+    
+    const getNewestUserData = async () => {
+        try {
+            const response = await fetch(
+                'https://backend-answer-keys.onrender.com/get-newest-user',
+                {
+                    method: "GET",
+                }
+            );
+            const data = await response.json();
+            const userData = data[0];
+            setNewestUserData(userData);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+     useEffect(() => {
+    getNewestUserData();
+     }, []);
+    
+    const matchedSavedCountries = savedCountries.map((item) => {
+        return countriesData.find(
+            (country) => country.name?.common === item.country_name
+        );
+    })
+        
     // this function runs when the form is submitted...
     // the e.prevent stops page from refreshing
     function handleSubmit(e) {
@@ -32,10 +77,26 @@ function SavedCountries() {
     // this is the jsx that returns what to be displayed on page..
     return (
         <main className="page">
-            <h3 className="page-title">My Saved Countries</h3>
+            <h2 className="page-title">My Saved Countries</h2>
+            <section className="countries-section">
+                {matchedSavedCountries.map((country) => (
+                    <div className="country-card" key={country.cca3}>
+                        <img className="country-flag" src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+                        <h3>{country.name.common}</h3>
+                        <p>Population: {country.population}</p>
+                        <p>Region: {country.region}</p>
+                        <p>Capital: {country.capital?.[0]}</p>
+                    </div>
+                ))}
+            </section>
+            {newestUserData && (
+                <h2 className="welcome-message">
+                    Welcome, {newestUserData.name}!</h2>
+            )}
+            
             <section className="saved-layout">
                 <div className="profile-card">
-                    <h2 className="section-title">My Profile</h2>
+                  <h2 className="section-title">My Profile</h2>
                     <form className="profile-form" onSubmit={handleSubmit}>
                         <input type="text" name="fullName" placeholder="Full Name" value={formData.fullName} onChange={handleChange}/>
                         <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange}/>
