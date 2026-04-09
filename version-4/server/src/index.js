f991bb62a3466ccc571f5a2a5380b150b942b95c
jaclyn16 <jgrix16@yahoo.com>
2026-04-09 10:37:27 -0700

    removed config import

--- version-4/server/src/index.js
+++ version-4/server/src/index.js
@@ -6,7 +6,7 @@
 
 import express from "express";
 import pg from "pg";
-import config from "./config.js"; //importing database connection string..
+
 
 const db = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
